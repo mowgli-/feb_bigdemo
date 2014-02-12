d7f66accc521580ff7038fa7c6604768e1d42e63
nick n <nicckk@gmail.com>
2014-02-12 16:02:12 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FebBigdemo::Application.config.secret_key_base = 'c4483aa5eb7caa9e7cb0e7471db42d017ad70ee50ca6bdea9ceabd102f995244145b521f1ff5feb5123bf7897d66be21648a1119226f9f1259d98dd939a48d43'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FebBigdemo::Application.config.session_store :cookie_store, key: '_feb_bigdemo_session'
